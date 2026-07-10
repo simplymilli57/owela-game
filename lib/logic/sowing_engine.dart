303528ac4bdf93677b69095bb446a5a7c7657e58
simplymilli57 <simplymilli57@gmail.com>
2026-07-10 20:47:14 +0200

    Update sowing_engine.dart

--- lib/logic/sowing_engine.dart
+++ lib/logic/sowing_engine.dart
@@ -37,3 +37,36 @@ class StartingPosition {
     return board;
   }
 }
+class SowingEngine {
+
+  int lastLandingHole = -1;
+
+  List<int> sow(List<int> seeds, int startHole) {
+
+    int movingSeeds = seeds[startHole];
+
+    seeds[startHole] = 0;
+
+    int currentHole = startHole;
+
+    while (movingSeeds > 0) {
+
+      currentHole = (currentHole + 1) % 60;
+
+      lastLandingHole = currentHole;
+
+      seeds[currentHole]++;
+
+      movingSeeds--;
+
+      // Continue sowing if the last seed lands
+      // in a hole that already has seeds
+      if (movingSeeds == 0 && seeds[currentHole] > 1) {
+        movingSeeds = seeds[currentHole];
+        seeds[currentHole] = 0;
+      }
+    }
+
+    return seeds;
+  }
+}
