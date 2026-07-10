ada37bbc9e0ec2052e9e32be53f63158cc7951b7
simplymilli57 <simplymilli57@gmail.com>
2026-07-10 20:46:27 +0200

    Update starting_position.dart

--- lib/logic/starting_position.dart
+++ lib/logic/starting_position.dart
@@ -1,39 +1,33 @@
-import '../models/hole.dart';
+class SowingEngine {
 
-class StartingPosition {
-  static List<Hole> createBoard() {
-    List<Hole> board = [];
+  int lastLandingHole = -1;
 
-    for (int i = 1; i <= 60; i++) {
-      int seeds = 0;
+  List<int> sow(List<int> seeds, int startHole) {
 
-      // Player 1 starting seeds
-      if (i >= 1 && i <= 15) {
-        seeds = 2;
+    int movingSeeds = seeds[startHole];
+
+    seeds[startHole] = 0;
+
+    int currentHole = startHole;
+
+    while (movingSeeds > 0) {
+
+      currentHole = (currentHole + 1) % 60;
+
+      lastLandingHole = currentHole;
+
+      seeds[currentHole]++;
+
+      movingSeeds--;
+
+      // Continue sowing if the last seed lands
+      // in a hole that already has seeds
+      if (movingSeeds == 0 && seeds[currentHole] > 1) {
+        movingSeeds = seeds[currentHole];
+        seeds[currentHole] = 0;
       }
-
-      if (i >= 16 && i <= 19) {
-        seeds = 2;
-      }
-
-      // Player 2 starting seeds (mirrored)
-      if (i >= 46 && i <= 60) {
-        seeds = 2;
-      }
-
-      if (i >= 42 && i <= 45) {
-        seeds = 2;
-      }
-
-      board.add(
-        Hole(
-          id: i,
-          seeds: seeds,
-          owner: i <= 30 ? 1 : 2,
-        ),
-      );
     }
 
-    return board;
+    return seeds;
   }
 }
