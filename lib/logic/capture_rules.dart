9c939612730b5026037c438414cbe52502f247a4
simplymilli57 <simplymilli57@gmail.com>
2026-07-10 20:57:03 +0200

    Update capture_rules.dart

--- lib/logic/capture_rules.dart
+++ lib/logic/capture_rules.dart
@@ -1,12 +1,41 @@
 class CaptureRules {
 
-  bool canCapture(int holeIndex, int seedsInHole) {
-    // A capture requires the landing hole to contain seeds
-    return seedsInHole > 0;
+  int oppositeHole(int holeIndex) {
+    return (holeIndex + 30) % 60;
   }
 
-  int oppositeHole(int holeIndex) {
-    // Opposite side of the 60-hole board
-    return (holeIndex + 30) % 60;
+  bool canCapture(
+    int landingHole,
+    List<int> seeds,
+    int currentPlayer,
+  ) {
+
+    // The landing hole must have seeds
+    if (seeds[landingHole] == 0) {
+      return false;
+    }
+
+    // Find opponent's opposite hole
+    int opposite = oppositeHole(landingHole);
+
+    // Opponent must have seeds to capture
+    if (seeds[opposite] == 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  List<int> capture(
+    int landingHole,
+    List<int> seeds,
+  ) {
+
+    int opposite = oppositeHole(landingHole);
+
+    // Remove captured seeds
+    seeds[opposite] = 0;
+
+    return seeds;
   }
 }
