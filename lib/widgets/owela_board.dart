76327973e4aa142f94402e911a0d3569ab9c0a2d
simplymilli57 <simplymilli57@gmail.com>
2026-07-10 19:58:05 +0200

    Update owela_board.dart

--- lib/widgets/owela_board.dart
+++ lib/widgets/owela_board.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 
 class OwelaBoard extends StatelessWidget {
-  const OwelaBoard({super.key});
+  final List<int> seeds;
+  final Function(int) onHoleTap;
+
+  const OwelaBoard({
+    super.key,
+    required this.seeds,
+    required this.onHoleTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    List<int> seeds = List.filled(60, 0);
-
     return GridView.builder(
       itemCount: 60,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 10,
       ),
       itemBuilder: (context, index) {
-        return Container(
-          margin: const EdgeInsets.all(4),
-          decoration: BoxDecoration(
-            border: Border.all(),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Center(
-            child: Text(
-              '${seeds[index]}',
-              style: const TextStyle(
-                fontSize: 20,
+        return GestureDetector(
+          onTap: () {
+            onHoleTap(index);
+          },
+          child: Container(
+            margin: const EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              border: Border.all(),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Center(
+              child: Text(
+                '${seeds[index]}',
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
           ),
